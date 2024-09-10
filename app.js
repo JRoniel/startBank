0c68a90026946f252b2829aa5f94f7d025b181dc
John Roniel <johnroniel8264@gmail.com>
2024-09-10 10:10:51 -0300

    <feat> Added CORS rule

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const port = 3000;
 // Configurando o express para aceitar JSON
 app.use(express.json());
 
+// CORS
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); //  Aceitar qualquer origem
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS"); //  Aceitar qualquer método
+    next();
+});
+
 // Configurando rotas com Consign
 consign()
     .include('routes')
